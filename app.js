ffb2149ff20bd0e3ebd9e5fca3e5475ed1cc0953
JMRosenberg <Jacob.Rosenberg@Tufts.edu>
2014-04-07 07:04:34 -0400

    initial get scores written

--- app.js
+++ app.js
@@ -5,17 +5,39 @@ var app = express();
 
 var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/mydb';
 
-mongo.Db.connect(mongoUri, function (err, db) {
+/*mongo.Db.connect(mongoUri, function (err, db) {
     db.collection('mydocs', function (er, collection) {
-	collection.insert({'mykey': 'myvalue'}, {safe: true}, function (er, rs) {
+	collection.insert({'numKey': 47}, {safe: true}, function (er, rs) {
 	});
     });
-});
+});*/
 
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
     res.send('Hello World!');
+});
+
+app.get('/scores.json', function(req, res) {
+    mongo.Db.connect(mongoUri, function (err, db) {
+	db.collection('scores', function (er, collection) {
+
+	    var c = collection.find({}).sort("score").toArray(function(errr, x){
+		res.send(x);
+	    });
+
+	});
+    });
+});
+
+app.post('/submit.json', function(req, res) {
+    mongo.Db.connect(mongoUri, function (err, db) {
+	db.collection('scores', function (er, collection) {
+
+	    
+
+	});
+    });
 });
 
 var port = Number(process.env.PORT || 5000);
