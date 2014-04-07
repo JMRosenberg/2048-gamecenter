3cfd089ac57b420cf31f1a639e6bb406dac627f7
JMRosenberg <Jacob.Rosenberg@Tufts.edu>
2014-04-07 07:39:01 -0400

    started on post submit

--- app.js
+++ app.js
@@ -1,18 +1,11 @@
 var express = require("express");
-var logfmt = require("logfmt");
 var mongo = require("mongodb");
 var app = express();
 
+
 var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/mydb';
 
-/*mongo.Db.connect(mongoUri, function (err, db) {
-    db.collection('mydocs', function (er, collection) {
-	collection.insert({'numKey': 47}, {safe: true}, function (er, rs) {
-	});
-    });
-});*/
-
-app.use(logfmt.requestLogger());
+app.use(express.bodyParser());
 
 app.get('/', function(req, res) {
     res.send('Hello World!');
@@ -20,7 +13,7 @@ app.get('/', function(req, res) {
 
 app.get('/scores.json', function(req, res) {
     mongo.Db.connect(mongoUri, function (err, db) {
-	db.collection('scores', function (er, collection) {
+	db.collection('scoresTemp', function (er, collection) {
 
 	    var c = collection.find({}).sort("score").toArray(function(errr, x){
 		res.send(x);
@@ -32,9 +25,14 @@ app.get('/scores.json', function(req, res) {
 
 app.post('/submit.json', function(req, res) {
     mongo.Db.connect(mongoUri, function (err, db) {
-	db.collection('scores', function (er, collection) {
+	db.collection('scoresTemp', function (er, collection) {
 
+	    console.log(req.body);
+	    var score = req.body.score;
+	    var username = req.body.username;
 	    
+	    collection.insert({"score": score, "username": username}, function (err, r){});
+	    res.send("Added");
 
 	});
     });
